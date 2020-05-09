36f3f55a46489bfbb16034f981c8a71d48dd46a1
HeoYongJe <hyj.campus@gmail.com>
2020-05-09 23:01:12 +0900

    closure add

--- js/closure.js
+++ js/closure.js
@@ -1,27 +1,54 @@
-let increaseBtn = document.getElementById('increase');
-let countAdd = document.getElementById('countAdd');
+// let increaseBtn = document.querySelector('.increase');
+// let countAdd = document.querySelector('.countAdd');
 
-let increase = (function(){
-    let counter = 0;
-    return function(){
-        return ++counter;
-    };
+// let increase = (function(){
+//     let counter = 0;
+//     return function(){
+//         return counter++;
+//     };
+// }());
+
+// increaseBtn.onclick = function(){
+//     countAdd.innerHTML = increase();
+// };
+
+// let decreaseBtn = document.querySelector('.decrease');
+// let countOut = document.querySelector('.countOut');
+
+// let decrease = (function(){
+//     let counter = 0;
+//     return function(){
+//         return --counter;
+//     };
+// }());
+
+// decreaseBtn.onclick = function(){
+//     countOut.innerHTML = decrease();
+// };
+
+const $counter = document.querySelector('.counter');
+
+const Counter = (function () {
+
+  let num = 0;
+
+  function Counter() {
+
+  }
+
+  Counter.prototype.increase = function () {
+    $counter.textContent = ++num;
+  };
+
+  Counter.prototype.decrease = function () {
+    if (num <= 0) return;
+    $counter.textContent = --num;
+  };
+
+  return Counter;
 }());
 
-increaseBtn.onclick = function(){
-    countAdd.innerHTML = increase();
-};
+const counter = new Counter();
 
-let decreaseBtn = document.getElementById('decrease');
-let countOut = document.getElementById('countOut');
-
-let decrease = (function(){
-    let counter = 0;
-    return function(){
-        return --counter;
-    };
-}());
-
-decreaseBtn.onclick = function(){
-    countOut.innerHTML = decrease();
-};
+document.querySelector('.increase').onclick = counter.increase;
+document.querySelector('.decrease').onclick = counter.decrease;
