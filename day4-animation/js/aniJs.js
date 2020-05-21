57ed3271be6f9c15e768f7b4b296261025c96e59
HeoYongJe <hyj.campus@gmail.com>
2020-05-21 17:09:50 +0900

    animation modification

--- day4-animation/js/aniJs.js
+++ day4-animation/js/aniJs.js
@@ -1,17 +1,22 @@
-const $aniBoxArea = document.getElementById('aniBoxArea');
+// const $aniBoxArea = document.getElementById('aniBoxArea');
 const $aniBox = document.querySelector('.aniBox');
+const aniClick = $aniBox;
 
-$aniBox.onclick = function(rotate){
-    if(rotate){
-        this.style.transform = 'rotate(180deg)';
-        this.style.background = 'yellow';
-        this.style.transtion = '1s';
+$aniBox.onclick = function() {
+    for(let i = 0; i < $aniBox.length; i++){
+        if(aniClick > 1){
+            aniClick.style.transform = 'rotate(180deg)';
+            aniClick.style.background = 'yellow';
+            aniClick.style.transtion = '1s';
+        }
+        else{
+            aniClick.style.transform = 'rotate(-180deg)';
+            aniClick.style.background = 'orange';                
+        }
     }
-    else{
-        this.style.transform = 'rotate(-180deg)';
-        this.style.background = 'orange';
-    }
-}
-const $aniBoxchildren = new rotate;
+        
+;}
+aniClick();
+
 
 
