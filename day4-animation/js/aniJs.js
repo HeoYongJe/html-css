c613b5d7ffef5bcd82a0d5e6d7ba708240c9da4a
HeoYongJe <hyj.campus@gmail.com>
2020-05-29 23:15:18 +0900

    -

--- day4-animation/js/aniJs.js
+++ day4-animation/js/aniJs.js
@@ -4,25 +4,22 @@ const $aniBox3 = document.querySelector('.aniBox3');
 const $aniBox4 = document.querySelector('.aniBox4');
 const $aniBoxArea = document.getElementById('aniBoxArea');
 // const aniClick = $aniBox;
-
-$aniBox1.onclick = function() {
+const isListOpen = () => ([...$aniBoxArea > li.classList].includes('listOpen'));
+function setListOpenStatus() {
     for(let i = 0; i < 1; i++){
         if(i < 1){
             this.style.transform = 'rotate(180deg)';
             this.style.background = 'yellow';
             this.style.transtion = '1s';
         }
+        else{
+            this.style.transform = 'rotate(-180deg)';
+            this.style.background = 'blue';                
+        }
     }      
 };
-// $aniBox1.onclick = function() {
-//     for(let i = 0; i < 1; i++){
-//         if(i < 0){
-//             this.style.transform = 'rotate(-180deg)';
-//             this.style.background = 'orange'; 
-//         }
-//     }      
-// };
 
+$aniBoxArea.addEventListener('click', () => setListOpenStatus(isListOpen()));
 // console.log(event.target)
 
 // $aniBox.addEventListener('click', event => {
