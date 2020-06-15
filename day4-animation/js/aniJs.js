9d145856cc660ab998c5e83c95aab332436095a8
HeoYongJe <hyj.campus@gmail.com>
2020-06-15 23:15:52 +0900

    add

--- day4-animation/js/aniJs.js
+++ day4-animation/js/aniJs.js
@@ -1,37 +1,24 @@
+const $aniBoxLi = document.querySelectorAll('.aniBox')
 const $aniBox1 = document.querySelector('.aniBox1');
 const $aniBox2 = document.querySelector('.aniBox2');
 const $aniBox3 = document.querySelector('.aniBox3');
 const $aniBox4 = document.querySelector('.aniBox4');
 const $aniBoxArea = document.getElementById('aniBoxArea');
+
 // const aniClick = $aniBox;
-const isListOpen = () => ([...$aniBoxArea > li.classList].includes('listOpen'));
-function setListOpenStatus() {
+// const isListOpen = () => ([...$aniBoxArea > li.classList].includes('listOpen'));
+$aniBoxLi.mousenter = function() {
+
     for(let i = 0; i < $aniBoxArea.length; i++){
-        if(){
+        if(i < 1){
             this.style.transform = 'rotate(180deg)';
             this.style.background = 'yellow';
             this.style.transtion = '1s';
         }
         else{
+            $aniBoxLi.mouseleave;
             this.style.transform = 'rotate(-180deg)';
             this.style.background = 'blue';                
         }
-    }      
+    };
 };
-
-$aniBoxArea.addEventListener('click', () => setListOpenStatus(isListOpen()));
-// console.log(event.target)
-
-// $aniBox.addEventListener('click', event => {
-//     for(let i = 0; i < $aniBox.length; i++){
-//         if(){
-//             this.style.transform = 'rotate(180deg)';
-//             this.style.background = 'yellow';
-//             this.style.transtion = '1s';
-//         }
-//         else{
-//             this.style.transform = 'rotate(-180deg)';
-//             this.style.background = 'orange';                
-//         }
-//     }      
-// });
